9a5e981394c39451eaea4ed280652fa892546672
Sekera Alexandr <sekera.alexandr.2017@skola.ssps.cz>
2021-01-21 19:59:41 +0100

    Funkční checkboxy, bez awaitu.

--- Prvocisla/MainWindow.xaml.cs
+++ Prvocisla/MainWindow.xaml.cs
@@ -26,15 +26,69 @@ namespace Prvocisla
             InitializeComponent();
         }
         Prvocislo p;
+        string[] PoleCisel;
+        string Trojky = string.Empty;
+        string SjednocenaCisla = string.Empty;
         private void TextBox_KeyDown(object sender, KeyEventArgs e)
         {
             if (e.Key == Key.Enter)
             {
                 p = new Prvocislo(Convert.ToInt32(HorniMez.Text));
-                p.zobrazit();
+                p.ShowPrime(Convert.ToInt32(DolniMez.Text));
                 PrvocislaVypsana.Text = p.xp;
+                PoleCisel = p.xp.Split(' ');
                 p.xp = "";
+                if (CB3.IsChecked == true && CBMany.IsChecked == false)
+                {
+                    Trojky = VypisCislici3(PoleCisel);
+                    PrvocislaVypsana.Text = Trojky;
+                }
+                else if (CB3.IsChecked == false && CBMany.IsChecked == true)
+                {
+                    SjednocenaCisla = VypisSjednoceneCislice(PoleCisel);
+                    PrvocislaVypsana.Text = SjednocenaCisla;
+                }
+                else if (CB3.IsChecked == true && CBMany.IsChecked == true)
+                {
+                    SjednocenaCisla = VypisSjednoceneCislice(PoleCisel);
+                    Trojky = VypisCislici3(SjednocenaCisla.Split(' '));
+                    PrvocislaVypsana.Text = Trojky;
+                }
+                Trojky = "";
+                SjednocenaCisla = "";
             }
+        }
+        string VypisCislici3(string[] CiselnePole) 
+        {
+            string Cisla = string.Empty;
+            for (int i = 0; i < CiselnePole.Length; i++)
+            {
+                if (CiselnePole[i].Contains("3")) Cisla += $"{CiselnePole[i]} ";
+            }
+            return Cisla;
+        }
+        string VypisSjednoceneCislice(string[] CiselnePole) 
+        {
+            string Cisla = string.Empty;
+            char[] Cislice; //char, ve kterém bude číslo rozděleno na samostatné číslice
+            for (int i = 0; i < CiselnePole.Length - 1; i++)
+            {
+                    Cislice = CiselnePole[i].ToCharArray(); //Zde je provedeno rozložení čísla do charu
+                    for (int j = 0; j < Cislice.Length; j++)
+                    {
+                        if (j != Cislice.Length - 1) //Zjištění, jestli se vejdu do pole
+                        {
+                            if (Cislice[j] == Cislice[j + 1])
+                            {
+                                if (j != Cislice.Length - 2) //Zjištění, jestli se vejdu do pole
+                                {
+                                    if (Cislice[j + 1] == Cislice[j + 2]) Cisla += $"{PoleCisel[i]} ";
+                                }
+                            }
+                        }
+                    }
+            }
+            return Cisla;
         }
 
         Regex JsouCisla = new Regex("^[0-9]*$");
@@ -47,18 +101,18 @@ namespace Prvocisla
     {
         bool[] je_prv; // Bool pole (rozhodování, zda je číslo prvočíslo)
         public string xp = string.Empty;
-        public Prvocislo(int MAX)
+        public Prvocislo(int horniMez)
         {
-            je_prv = new bool[MAX + 1]; // Vytvoříme pole s horní mezí
-            for (int i = 2; i <= MAX; i++)
+            je_prv = new bool[horniMez + 1]; // Vytvoříme pole s horní mezí
+            for (int i = 2; i <= horniMez; i++)
             {
                 je_prv[i] = true; // Všechna čísla nastavíme na prvočísla
             }
-            for (int i = 2; i <= MAX; i++)
+            for (int i = 2; i <= horniMez; i++)
             {
                 if (je_prv[i])
                 {
-                    for (int j = 2 * i; j <= MAX; j += i)
+                    for (int j = 2 * i; j <= horniMez; j += i)
                     {
                         je_prv[j] = false; // Vyhodnotíme, zda je číslo skutečně prvočíslo
                     }
@@ -67,11 +121,12 @@ namespace Prvocisla
         }
 
         
-        public void zobrazit()
+        public void ShowPrime(int dolniMez)
         {
-            for (int i = 0; i < je_prv.Length; i++)
+            if (dolniMez < 0 || dolniMez == 0 || dolniMez == 1) dolniMez = 2;
+            for (int i = dolniMez; i < je_prv.Length; i++)
             {
-                if (je_prv[i]) xp += $" {i}"; // Uložíme do stringu čísla, která jsou prvočísly (true)
+                if (je_prv[i]) xp += $"{i} "; // Uložíme do stringu čísla, která jsou prvočísly (true)
             }
         }
     }
